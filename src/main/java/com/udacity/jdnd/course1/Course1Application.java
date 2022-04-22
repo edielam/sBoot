95d8c7d2a128e76e3981b5d44dd6fb40310e9428
edielam <edguard738@gmail.com>
2022-04-22 15:16:57 +0000

    spring boot config

--- src/main/java/com/udacity/jdnd/course1/Course1Application.java
+++ src/main/java/com/udacity/jdnd/course1/Course1Application.java
@@ -2,8 +2,9 @@ package com.udacity.jdnd.course1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication //(exclude = {DataSourceAutoConfiguration.class })
 public class Course1Application {
 
 	public static void main(String[] args) {
