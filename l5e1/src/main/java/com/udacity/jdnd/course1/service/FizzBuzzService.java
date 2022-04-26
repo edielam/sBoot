98339990643c82f517f3f4d292c700d8a8172d72
edielam <edguard738@gmail.com>
2022-04-26 13:56:57 +0000

    fizzbuzz tests

--- l5e1/src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
+++ l5e1/src/main/java/com/udacity/jdnd/course1/service/FizzBuzzService.java
@@ -10,6 +10,17 @@ public class FizzBuzzService {
      * @Throws IllegalArgumentException for values < 1
      */
     public String fizzBuzz(int number) {
-        return "";
+        if(number < 1){
+            throw new IllegalArgumentException("Value must be greater than 0");
+        }
+        if(number % 5 == 0 && number % 3==0){
+            return "FizzBuzz";
+        } else if( number % 5==0){
+            return "Buzz";
+        } else if(number % 3 ==0){
+            return "Fizz";
+        } else {
+            return "" + number;
+        }
     }
 }
