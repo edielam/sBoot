7ce42c876ee5f5f070243bb326438cb07f8641b5
edielam <edguard738@gmail.com>
2022-07-13 12:12:19 +0000

    dog api

--- bootstrap/src/main/java/com/udacity/restfulapi/entity/Location.java
+++ bootstrap/src/main/java/com/udacity/restfulapi/entity/Location.java
@@ -13,6 +13,9 @@ public class Location {
     private String name;
     private String address;
 
+    public Location() {
+    }
+
     public Location(Long id, String name, String address) {
         this.id = id;
         this.name = name;
@@ -21,6 +24,30 @@ public class Location {
 
     public Location(String name, String address) {
         this.name = name;
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
         this.address = address;
     }
 }
