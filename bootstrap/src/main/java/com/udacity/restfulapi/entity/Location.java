932cb97574d04d7b016fa41bb10613c5a0908260
edielam <edguard738@gmail.com>
2022-07-13 12:10:37 +0000

    dog api

---
+++ bootstrap/src/main/java/com/udacity/restfulapi/entity/Location.java
@@ -0,0 +1,26 @@
+package com.udacity.restfulapi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String address;
+
+    public Location(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+    }
+
+    public Location(String name, String address) {
+        this.name = name;
+        this.address = address;
+    }
+}
