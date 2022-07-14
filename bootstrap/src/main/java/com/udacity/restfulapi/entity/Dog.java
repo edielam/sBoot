d6b2617a0875f33fbd3b505a7aaf9bb8fd1fb8c6
edielam <edguard738@gmail.com>
2022-07-14 13:59:02 +0000

    Dog api controller.i.e.restAPI

--- bootstrap/src/main/java/com/udacity/restfulapi/entity/Dog.java
+++ bootstrap/src/main/java/com/udacity/restfulapi/entity/Dog.java
@@ -23,5 +23,42 @@ public class Dog {
         this.breed = breed;
         this.origin = origin;
     }
-    
+
+    public Dog(String name, String breed, String origin) {
+        this.name = name;
+        this.breed = breed;
+        this.origin = origin;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
 }
---
+++ bootstrap/src/main/java/com/udacity/restfulapi/web/DogController.java
@@ -0,0 +1,8 @@
+package com.udacity.restfulapi.web;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DogController {
+
+}
