49206d2d4d8c4fe95e2ed60ff068c8027c4e955c
edielam <edguard738@gmail.com>
2022-07-22 21:50:21 +0000

    dogGraphQlapi

--- doggraphql/src/main/java/com/doggraphql/api/service/DogNotFoundException.java
+++ doggraphql/src/main/java/com/doggraphql/api/service/DogNotFoundException.java
@@ -1,4 +1,14 @@
 package com.doggraphql.api.service;
 
-public class DogNotFoundException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Your dog is not here")
+public class DogNotFoundException extends RuntimeException{
+    public DogNotFoundException() {
+    }
+
+    public DogNotFoundException(String message) {
+        super(message);
+    }
 }
