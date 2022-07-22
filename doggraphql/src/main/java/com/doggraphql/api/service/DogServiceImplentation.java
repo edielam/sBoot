9de551e815caa0dbae60134f5695b76b7fd579de
edielam <edguard738@gmail.com>
2022-07-22 22:00:05 +0000

    dogGraphQlapi

--- doggraphql/src/main/java/com/doggraphql/api/service/DogServiceImplentation.java
+++ doggraphql/src/main/java/com/doggraphql/api/service/DogServiceImplentation.java
@@ -1,4 +1,33 @@
 package com.doggraphql.api.service;
 
-public class DogServiceImplentation {
+import com.doggraphql.api.entity.Dog;
+import com.doggraphql.api.repository.DogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DogServiceImplentation implements DogService {
+    @Autowired
+    DogRepository dogRepository;
+
+    public List<Dog> retrieveDogs() {
+        return (List<Dog>) dogRepository.findAll();
+    }
+
+    public List<String> retrieveDogBreed() {
+        return (List<String>) dogRepository.findAllBreed();
+    }
+
+    public String retrieveDogById(Long id) {
+        Optional<String> optionalBreed = Optional.ofNullable(dogRepository.findBreedById(id));
+        String breed = optionalBreed.orElseThrow(DogNotFoundException::new);
+        return breed;
+    }
+
+    public List<String> retrieveDogNames() {
+        return (List<String>) dogRepository.findAllName();
+    }
 }
