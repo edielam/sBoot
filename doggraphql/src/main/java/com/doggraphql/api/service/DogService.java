aebc11261c67e06083c56c8d8ee053907f95b5e7
edielam <edguard738@gmail.com>
2022-07-22 21:50:52 +0000

    dogGraphQlapi

--- doggraphql/src/main/java/com/doggraphql/api/service/DogService.java
+++ doggraphql/src/main/java/com/doggraphql/api/service/DogService.java
@@ -1,4 +1,13 @@
 package com.doggraphql.api.service;
 
+import com.doggraphql.api.entity.Dog;
+
+import java.util.List;
+
 public interface DogService {
+    List<Dog> retrieveDogs();
+    List<String> retrieveDogBreed();
+    String retrieveDogById(Long id);
+    List<String> retrieveDogNames();
+
 }
