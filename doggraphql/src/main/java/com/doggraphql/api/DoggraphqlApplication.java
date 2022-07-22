e5b57633b501fa695cc482f425f6a7918d78ea3f
edielam <edguard738@gmail.com>
2022-07-22 18:59:38 +0000

    dogGraphQlapi

---
+++ doggraphql/src/main/java/com/doggraphql/api/DoggraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.doggraphql.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoggraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoggraphqlApplication.class, args);
+	}
+
+}
