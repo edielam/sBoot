5cf9aca800ab48dd3bd792d6a277f332057f9929
edielam <edguard738@gmail.com>
2022-07-23 14:54:13 +0000

    resolved graphql version problem

--- doggraphql/src/main/java/com/doggraphql/api/exception/DogNotFoundException.java
+++ doggraphql/src/main/java/com/doggraphql/api/exception/DogNotFoundException.java
@@ -1,10 +1,11 @@
-package com.doggraphql.api.service;
+package com.doggraphql.api.exception;
 
+import Graphql;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Your dog is not here")
-public class DogNotFoundException extends RuntimeException{
+public class DogNotFoundException extends RuntimeException {
     public DogNotFoundException() {
     }
 
---
+++ doggraphql/src/main/java/com/doggraphql/api/resolver/Query.java
@@ -0,0 +1,31 @@
+package com.doggraphql.api.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.doggraphql.api.entity.Dog;
+import com.doggraphql.api.exception.DogNotFoundException;
+import com.doggraphql.api.repository.DogRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class Query implements GraphQLQueryResolver {
+    private DogRepository dogRepository;
+
+    public Query(DogRepository dogRepository) {
+        this.dogRepository = dogRepository;
+    }
+
+    public Iterable<Dog> findAllDogs() {
+        return dogRepository.findAll();
+    }
+
+    public Dog findDogById(Long id) {
+        Optional<Dog> optionalDog = dogRepository.findById(id);
+        if (optionalDog.isPresent()) {
+            return optionalDog.get();
+        } else {
+            throw new DogNotFoundException("Dog Not Found", id);
+        }
+    }
+}
