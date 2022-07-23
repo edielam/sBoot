913f9c6c9374b680bb9dfd8d008b0556d5da46b0
edielam <edguard738@gmail.com>
2022-07-23 14:59:18 +0000

    resolved graphql version problem

--- doggraphql/src/main/java/com/doggraphql/api/exception/DogNotFoundException.java
+++ doggraphql/src/main/java/com/doggraphql/api/exception/DogNotFoundException.java
@@ -1,15 +1,36 @@
 package com.doggraphql.api.exception;
 
-import Graphql;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import graphql.ErrorType;
+import graphql.GraphQLError;
+import graphql.language.SourceLocation;
 
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Your dog is not here")
-public class DogNotFoundException extends RuntimeException {
-    public DogNotFoundException() {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+;
+
+public class DogNotFoundException extends RuntimeException implements GraphQLError {
+
+    private Map<String, Object> extensions = new HashMap<>();
+
+    public DogNotFoundException(String message, Long invalidDogId) {
+        super(message);
+        extensions.put("invalidDogId", invalidDogId);
     }
 
-    public DogNotFoundException(String message) {
-        super(message);
+    @Override
+    public List<SourceLocation> getLocations() {
+        return null;
+    }
+
+    @Override
+    public Map<String, Object> getExtensions() {
+        return extensions;
+    }
+
+    @Override
+    public ErrorType getErrorType() {
+        return ErrorType.DataFetchingException;
     }
 }
