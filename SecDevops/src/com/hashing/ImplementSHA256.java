29e69e7b672ab47d407ff2c4ffe93329949bbdd5
edielam <edguard738@gmail.com>
2022-07-29 22:10:51 +0000

    hashing

---
+++ SecDevops/src/com/hashing/ImplementSHA256.java
@@ -0,0 +1,40 @@
+package com.hashing;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class ImplementSHA256 {
+    public static void main(String[] args) {
+        String myPassword = "yooWhatsapp";
+        String securePassword =
+    }
+    // Method to generate a Salt
+    private static byte[] createSalt() {
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        return salt;
+    }
+
+    // Method to generate the hash.
+//It takes a password and the Salt as input arguments
+    private static String get_SecurePassword(String passwordToHash){
+        String generatedPassword = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            //md.update(salt);
+            byte[] bytes = md.digest(passwordToHash.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for(int i=0; i< bytes.length ;i++)
+            {
+                sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+            }
+            generatedPassword = sb.toString();
+        }
+        catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+        return generatedPassword;
+    }
+}
