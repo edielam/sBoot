43731f6d41a8cb994966950d9fc6aa416af9c155
edielam <edguard738@gmail.com>
2022-08-12 23:58:30 +0000

    command line runner

--- cardatabase/src/main/java/com/monolith/cardatabase/CarDatabaseApplication.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/CarDatabaseApplication.java
@@ -1,5 +1,8 @@
 package com.monolith.cardatabase;
 
+import com.monolith.cardatabase.domain.Car;
+import com.monolith.cardatabase.domain.CarRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.slf4j.*;
@@ -8,12 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CarDatabaseApplication implements CommandLineRunner {
 	final Logger log = LoggerFactory.getLogger(CarDatabaseApplication.class);
+	@Autowired
+	private CarRepository carRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(CarDatabaseApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		carRepository.save(new Car("Ford","Mustang","Red","ADF-1121",
+				2022, 59000));
 	}
 }
