0babdfbdc50d6975164e37dc4bca5ffe7c7099ad
edielam <edguard738@gmail.com>
2022-08-12 15:29:53 +0000

    newproject

--- cardatabase/src/main/java/com/monolith/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/domain/Car.java
@@ -1,2 +1,17 @@
-package com.monolith.cardatabase.domain;public class Car {
+package com.monolith.cardatabase.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String brand, model, color, registerNumber;
+    private int year;
+    private double price;
+    @Column(name = "Explanation", nullable = false)
+    private String description;
+
+
 }
