339ace101da6c11a7e9bd1899558f679121135c9
edielam <edguard738@gmail.com>
2022-08-12 15:32:48 +0000

    data jpa

--- cardatabase/src/main/java/com/monolith/cardatabase/domain/Car.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/domain/Car.java
@@ -13,5 +13,16 @@ public class Car {
     @Column(name = "Explanation", nullable = false, length = 256)
     private String description;
 
+    public Car() {
+    }
 
+    public Car(String brand, String model, String color, String registerNumber, int year, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.color = color;
+        this.registerNumber = registerNumber;
+        this.year = year;
+        this.price = price;
+    }
+    
 }
