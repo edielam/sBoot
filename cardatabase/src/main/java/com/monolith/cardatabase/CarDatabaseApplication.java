fcd40841d5f63051fbed2a272285df90b5661e65
edielam <edguard738@gmail.com>
2022-08-13 15:08:04 +0000

    data jpa

--- cardatabase/src/main/java/com/monolith/cardatabase/CarDatabaseApplication.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/CarDatabaseApplication.java
@@ -26,17 +26,20 @@ public class CarDatabaseApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Owner owner1 = new Owner("Eddie", "Lampoh");
-		Owner owner2 = new Owner("Richie", "O'brain");
-		ownerRepository.saveAll(Arrays.asList(owner1, owner2));
+		Owner owner2 = new Owner("Richie", "O'Brain");
+		Owner owner3 = new Owner("Ivy", "Sweet");
+		Owner owner4 = new Owner("Mel", "Darling");
+		ownerRepository.saveAll(Arrays.asList(owner1, owner2, owner3, owner4));
 
 		Car car1 = new Car("Ford","Mustang","Red","ADF-1121",
-				2022, 59000, owner1));
-		carRepository.save(new Car("Nissan", "Leaf","White",
-				"SSJ-3002",2019,28000, o));
-		carRepository.save(new Car("Toyota", "Prius","silver","KKO-0212",
-				2020,39000));
-		carRepository.save(new Car("Toyota", "Rav4","blue","KKO-0212",
-				2020,39000));
+				2022, 59000, owner1);
+		Car car2 = new Car("Nissan", "Leaf","White",
+				"SSJ-3002",2019,28000, owner2);
+		Car car3 = new Car("Toyota", "Prius","silver","KKO-0212",
+				2020,39000, owner3);
+		Car car4 = new Car("Toyota", "Rav4","blue","KKO-0212",
+				2020,39000, owner4);
+		carRepository.saveAll(Arrays.asList(car1,car2,car3,car4));
 
 		for (Car car : carRepository.findAll()){
 			log.info(car.getBrand() +" "+ car.getModel());
