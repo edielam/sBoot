4588460f57fa137a91fde37916560aa342e6fb6b
edielam <edguard738@gmail.com>
2022-08-13 18:42:46 +0000

    security and testing

--- cardatabase/src/main/java/com/monolith/cardatabase/domain/User.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/domain/User.java
@@ -6,10 +6,10 @@ import javax.persistence.*;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
+    @Column(nullable = false, updatable = false)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String password;
 
     @Column(nullable = false)
