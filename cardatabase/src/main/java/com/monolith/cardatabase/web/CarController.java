e010777177082a50a876211b3fd8e8637bb0e5c3
edielam <edguard738@gmail.com>
2022-08-13 16:22:29 +0000

    web controollers

--- cardatabase/src/main/java/com/monolith/cardatabase/web/CarController.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/web/CarController.java
@@ -1,4 +1,13 @@
 package com.monolith.cardatabase.web;
 
+import com.monolith.cardatabase.domain.Car;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CarController {
+    @RequestMapping("/cars")
+    public Iterable<Car> getCars(){
+
+    }
 }
