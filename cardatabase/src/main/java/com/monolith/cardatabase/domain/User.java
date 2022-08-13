d0826848d92b35a8f1caa57e5f6257de3fbf56bc
edielam <edguard738@gmail.com>
2022-08-13 18:44:24 +0000

    security and testing

--- cardatabase/src/main/java/com/monolith/cardatabase/domain/User.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/domain/User.java
@@ -17,5 +17,44 @@ public class User {
 
     @Column(nullable = false)
     private String role;
+    public User() {
+    }
 
+    public User(String password, String username, String role) {
+        this.password = password;
+        this.username = username;
+        this.role = role;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
--- cardatabase/src/main/java/com/monolith/cardatabase/repository/UserRepository.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.monolith.cardatabase.repository;
 
+import com.monolith.cardatabase.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository {
+public interface UserRepository extends CrudRepository <User,Long> {
 }
