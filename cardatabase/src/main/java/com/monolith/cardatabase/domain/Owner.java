fe8103c795cde9a8798ae31248dba16a4f436099
edielam <edguard738@gmail.com>
2022-08-13 12:21:43 +0000

    owner entity

--- cardatabase/src/main/java/com/monolith/cardatabase/domain/Owner.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/domain/Owner.java
@@ -13,4 +13,36 @@ public class Owner {
     private Long ownerId;
     private String firstName;
     private String lastName;
+
+    public Owner() {
+    }
+
+    public Owner(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(Long ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
