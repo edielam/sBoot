5b0886b0e4322a88978b67e79ad2d5e69e4130b8
edielam <edguard738@gmail.com>
2022-08-13 15:11:11 +0000

    data jpa

--- cardatabase/src/main/java/com/monolith/cardatabase/CarDatabaseApplication.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/CarDatabaseApplication.java
@@ -32,11 +32,11 @@ public class CarDatabaseApplication implements CommandLineRunner {
 		ownerRepository.saveAll(Arrays.asList(owner1, owner2, owner3, owner4));
 
 		Car car1 = new Car("Ford","Mustang","Red","ADF-1121",
-				2022, 59000, owner1);
+				2022, 59000, owner3);
 		Car car2 = new Car("Nissan", "Leaf","White",
 				"SSJ-3002",2019,28000, owner2);
 		Car car3 = new Car("Toyota", "Prius","silver","KKO-0212",
-				2020,39000, owner3);
+				2020,39000, owner1);
 		Car car4 = new Car("Toyota", "Rav4","blue","KKO-0212",
 				2020,39000, owner4);
 		carRepository.saveAll(Arrays.asList(car1,car2,car3,car4));
