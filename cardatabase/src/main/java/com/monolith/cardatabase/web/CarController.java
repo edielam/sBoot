cc55448ed7cdfe88d2665cb924c3215443e3309b
edielam <edguard738@gmail.com>
2022-08-13 17:47:14 +0000

    spring data rest is really cool

--- cardatabase/src/main/java/com/monolith/cardatabase/web/CarController.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/web/CarController.java
@@ -4,7 +4,6 @@ import com.monolith.cardatabase.domain.Car;
 import com.monolith.cardatabase.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
