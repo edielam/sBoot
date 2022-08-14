03f73e1ae8d7e15caeed749fd4cdf1d5b0b512b7
edielam <edguard738@gmail.com>
2022-08-14 10:00:12 +0000

    security and testing

---
+++ cardatabase/src/main/java/com/monolith/cardatabase/services/UserDetailsServiceImpl.java
@@ -0,0 +1,33 @@
+package com.monolith.cardatabase.services;
+
+import com.monolith.cardatabase.domain.User;
+import com.monolith.cardatabase.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> userA = userRepository.findByUsername(username);
+        UserBuilder builder = null;
+        if(userA.isPresent()){
+            User currentUser = userA.get();
+            builder = org.springframework.security.core.userdetails.User.withUsername(username);
+            builder.password(currentUser.getPassword());
+            builder.roles(currentUser.getRole());
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+        return builder.build();
+    }
+}
