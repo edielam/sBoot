bf9247e38c71b79ac5adeccd5803da522b4a25d8
edielam <edguard738@gmail.com>
2022-08-14 11:18:05 +0000

    security and testing

--- cardatabase/src/main/java/com/monolith/cardatabase/web/LoginController.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/web/LoginController.java
@@ -3,6 +3,7 @@ package com.monolith.cardatabase.web;
 import com.monolith.cardatabase.domain.AccountCredentials;
 import com.monolith.cardatabase.services.JwtService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -29,6 +30,9 @@ public class LoginController {
         String jwts = jwtService.getToken(auth.getName());
 
         // Build response with the generated token
-        
+        return ResponseEntity.ok()
+                .header(HttpHeaders.AUTHORIZATION, "Bearer" +jwts)
+                .header(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "Authorization")
+                .build();
     }
 }
