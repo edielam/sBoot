a4ba6d587497843eeb9a03402799289db9522b03
edielam <edguard738@gmail.com>
2022-08-14 14:52:24 +0000

    adding jwt

--- cardatabase/src/main/java/com/monolith/cardatabase/config/SecurityConfig.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/config/SecurityConfig.java
@@ -2,11 +2,15 @@ package com.monolith.cardatabase.config;
 
 import com.monolith.cardatabase.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
@@ -21,5 +25,21 @@ public class SecurityConfig extends
             throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(new BCryptPasswordEncoder());
+    }
+    @Bean
+    public AuthenticationManager getAuthenticationManager() throws Exception {
+        return authenticationManager();
+    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                // POST request to /login endpoint is not secured
+                .antMatchers(HttpMethod.POST, "/login").
+                permitAll()
+                // All other requests are secured
+                .anyRequest().authenticated();
     }
 }
