28fcd5dcc378d023148c9d8e6bead093a912d34c
edielam <edguard738@gmail.com>
2022-08-14 10:26:47 +0000

    security and testing

--- cardatabase/src/main/java/com/monolith/cardatabase/repository/UserRepository.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/repository/UserRepository.java
@@ -3,9 +3,10 @@ package com.monolith.cardatabase.repository;
 import com.monolith.cardatabase.domain.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.Optional;
-
+@RepositoryRestResource(exported = false)
 public interface UserRepository extends CrudRepository <User,Long> {
     Optional<User> findByUsername(@Param("username") String username);
 }
