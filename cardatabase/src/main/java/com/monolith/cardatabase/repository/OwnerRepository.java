3b173d10eaf9b3314489355d5268eb04c37fb391
edielam <edguard738@gmail.com>
2022-08-15 02:48:39 +0000

    testing

--- cardatabase/src/main/java/com/monolith/cardatabase/repository/OwnerRepository.java
+++ cardatabase/src/main/java/com/monolith/cardatabase/repository/OwnerRepository.java
@@ -2,8 +2,12 @@ package com.monolith.cardatabase.repository;
 
 import com.monolith.cardatabase.domain.Owner;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "omegas")
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+    List<Owner> findByFirstName(@Param("firstName") String firstname);
 }
