2b479da031e074fcca8a730ce75a1a578c23cc8a
edielam <edguard738@gmail.com>
2022-08-23 13:16:48 +0000

    github api

--- cardatabase/restgithub/src/index.js
+++ cardatabase/restgithub/src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import Weather from './asyncAPI/weather';
 import reportWebVitals from './reportWebVitals';
+import Github from './asyncAPI/github';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Weather/>
+    <Github/>
   </React.StrictMode>
 );
 
