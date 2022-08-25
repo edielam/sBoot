36140ad49d30b950ee2d9f376239693f6687d543
edielam <edguard738@gmail.com>
2022-08-25 22:42:24 +0000

    shopping list

--- cardatabase/shoppinglist/src/App.js
+++ cardatabase/shoppinglist/src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
+import Container from '@mui/material/Container';
+import { AppBar, Typography, Toolbar } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello Ed</h1>
-    </div>
+    <Container>
+      <AppBar position='static'>
+        <Toolbar>
+          <Typography variant='h6'>
+            Shopping List
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Container>
   );
 }
 
