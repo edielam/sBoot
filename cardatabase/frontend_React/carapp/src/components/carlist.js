c2f25d506791c2f925e08873609865a240b45ef7
edielam <edguard738@gmail.com>
2022-08-26 15:52:12 +0000

    ryd garrage

--- cardatabase/frontend_React/carapp/src/components/carlist.js
+++ cardatabase/frontend_React/carapp/src/components/carlist.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { columns, SERVER_URL } from "../constants/constants";
+import { SERVER_URL } from "../constants/constants";
 import { DataGrid } from '@mui/x-data-grid';
+import { Button } from "@mui/material";
 
 function CarList(){
     const [cars, setCars] = useState([]);
@@ -10,7 +11,40 @@ function CarList(){
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err));
     }, []);
+    const columns = [
+        {field: 'brand', headerName: 'Brand', width: 200},
+        {field: 'model', headerName: 'Model', width: 200},
+        {field: 'color', headerName: 'Color', width: 200},
+        {field: 'registerNumber', headerName: 'Register Number', width: 200},
+        {field: 'years', headerName: 'Year', width: 150},
+        {field: 'price', headerName: 'Price', width: 150},
+        {
+            field: '_links.self.href', 
+            headerName: '', 
+            sortable: false,
+            filterable: false,
+            renderCell: row => 
+            <Button color='primary' variant="contained"
+            onClick={() => onDelClick(row.id)}>Delete
+            </Button>
+            }
+    ]
+
+    const fetchCars= () => {
+        fetch(SERVER_URL + 'api/cars')
+        .then(response => response.json())
+        .then(data => setCars(data._embedded.cars))
+        .catch(err => console.error(err));
+    }
+    useEffect(() => {
+        fetchCars();
+    }, []);
     
+    const onDelClick =(url) => {
+        fetch(url, {method: 'DELETE'})
+        .then(response => fetchCars())
+        .catch(err => console.error(err));
+    }
     return(
         <div  style={{ height: 500, width: '100%' }}>
             <DataGrid
--- cardatabase/frontend_React/carapp/src/constants/constants.js
+++ cardatabase/frontend_React/carapp/src/constants/constants.js
@@ -1,9 +1,1 @@
 export const SERVER_URL='http://localhost:8080/';
-export const columns = [
-    {field: 'brand', headerName: 'Brand', width: 200},
-    {field: 'model', headerName: 'Model', width: 200},
-    {field: 'color', headerName: 'Color', width: 200},
-    {field: 'registerNumber', headerName: 'Register Number', width: 200},
-    {field: 'years', headerName: 'Year', width: 150},
-    {field: 'price', headerName: 'Price', width: 150},
-]
