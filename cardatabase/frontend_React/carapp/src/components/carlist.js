785120438f20fe9834ceaed3f0a6c97d198ed145
edielam <edguard738@gmail.com>
2022-08-26 14:12:16 +0000

    ryd garrage

--- cardatabase/frontend_React/carapp/src/components/carlist.js
+++ cardatabase/frontend_React/carapp/src/components/carlist.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
+import { SERVER_URL } from "../constants/constants";
 
 function CarList(){
     const [cars, setCars] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:8080/api/cars')
+        fetch(SERVER_URL+'/api/cars')
         .then(response => response.json())
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err));
