2236159c72ac1228bf73af3f78736ace9b81d5d1
edielam <edguard738@gmail.com>
2022-08-26 14:36:01 +0000

    ryd garrage

--- cardatabase/frontend_React/carapp/src/components/carlist.js
+++ cardatabase/frontend_React/carapp/src/components/carlist.js
@@ -1,30 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { SERVER_URL } from "../constants/constants";
+import { columns, SERVER_URL } from "../constants/constants";
+import { DataGrid } from '@mui/x-data-grid';
 
 function CarList(){
     const [cars, setCars] = useState([]);
     useEffect(() => {
-        fetch(SERVER_URL+'/api/cars')
+        fetch(SERVER_URL + 'api/cars')
         .then(response => response.json())
         .then(data => setCars(data._embedded.cars))
         .catch(err => console.error(err));
     }, []);
+    
     return(
-        <div>
-            <table>
-                <tbody>
-                    {
-                        cars.map((car, index) => 
-                        <tr>
-                            <td>{car.brand}</td>
-                            <td>{car.model}</td>
-                            <td>{car.color}</td>
-                            <td>{car.year}</td>
-                            <td>{car.price}</td>
-                        </tr>)
-                    }
-                </tbody>
-            </table>
+        <div  style={{ height: 500, width: '100%' }}>
+            <DataGrid
+            rows={cars}
+            columns= {columns}
+            getRowId={row => row._links.self.href}/>
         </div>
     );
 }
--- cardatabase/frontend_React/carapp/src/constants/constants.js
+++ cardatabase/frontend_React/carapp/src/constants/constants.js
@@ -1,8 +1,9 @@
-export const SERVER_URL = 'http://localhost:8080/';
-export const COLUMNS = [
+export const SERVER_URL='http://localhost:8080/';
+export const columns = [
     {field: 'brand', headerName: 'Brand', width: 200},
     {field: 'model', headerName: 'Model', width: 200},
     {field: 'color', headerName: 'Color', width: 200},
-    {field: 'year', headerName: 'Year', width: 150},
+    {field: 'registerNumber', headerName: 'Register Number', width: 200},
+    {field: 'years', headerName: 'Year', width: 150},
     {field: 'price', headerName: 'Price', width: 150},
 ]
