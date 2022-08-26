f1a11175a0e39d1740564b8b9c8ef206a33d61a8
edielam <edguard738@gmail.com>
2022-08-26 20:35:10 +0000

    ryd garrage

--- cardatabase/frontend_React/carapp/src/components/addCar.js
+++ cardatabase/frontend_React/carapp/src/components/addCar.js
@@ -1,1 +1,12 @@
 import React from "react";
+import { Dialog, DialogContent,
+     DialogTitle, DialogActions } from "@mui/material";
+
+
+function AddCar(){
+    return(
+        <div>
+            
+        </div>
+    )
+}
