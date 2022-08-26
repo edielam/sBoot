d0290951ec6bc0a2721ff74a8f1fa3077913a8e3
edielam <edguard738@gmail.com>
2022-08-26 14:15:24 +0000

    ryd garrage

--- cardatabase/frontend_React/carapp/src/constants/constants.js
+++ cardatabase/frontend_React/carapp/src/constants/constants.js
@@ -1,1 +1,8 @@
 export const SERVER_URL = 'http://localhost:8080/';
+export const COLUMNS = [
+    {field: 'brand', headerName: 'Brand', width: 200},
+    {field: 'model', headerName: 'Model', width: 200},
+    {field: 'color', headerName: 'Color', width: 200},
+    {field: 'year', headerName: 'Year', width: 150},
+    {field: 'price', headerName: 'Price', width: 150},
+]
