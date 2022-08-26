6d41599f438f9e7d2500b58811eccc6d4a76d1da
edielam <edguard738@gmail.com>
2022-08-26 14:09:09 +0000

    ryd garrage

--- cardatabase/frontend_React/carapp/src/App.js
+++ cardatabase/frontend_React/carapp/src/App.js
@@ -3,17 +3,20 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import { Container } from '@mui/system';
+import CarList from './components/carlist';
 
 function App() {
   return (
-    <Container>
-      <AppBar>
+    <Container maxWidth="xl">
+      <AppBar position='static'>
         <Toolbar sx={{bgcolor: '#3E3B70'}}>
           <Typography variant='h6'>
             Ryd Garrage</Typography>
         </Toolbar>
-      </AppBar>
+      </AppBar><br/>
+      <CarList/>
     </Container>
+      
   );
 }
 
