cd796f8fac74ec4a12425e7684d50ad1fc109b95
edielam <edguard738@gmail.com>
2022-08-30 13:07:12 +0000

    ryd garrage

--- cardatabase/frontend_React/carapp/src/components/EditCar.js
+++ cardatabase/frontend_React/carapp/src/components/EditCar.js
@@ -3,3 +3,15 @@ import { Dialog, DialogContent,
      DialogTitle, DialogActions, Button, ThemeProvider, TextField } from "@mui/material";
 import { theme } from "../constants/constants";
 
+
+function EditCar(props){
+    const [open, setOpen] = useState(false);
+    const []
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default EditCar;
