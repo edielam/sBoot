9704f0c828c95de68d7f9d9790b3a5cb76ecf7e7
edielam <edguard738@gmail.com>
2022-09-02 13:59:47 +0000

    removed react favicon

--- cardatabase/frontend_React/carapp/src/App.test.js
+++ cardatabase/frontend_React/carapp/src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test('open add car modal form', () => {
+  render(<App/>);
+  fireEvent.click(screen.getByText('NEW CAR'))
 });
