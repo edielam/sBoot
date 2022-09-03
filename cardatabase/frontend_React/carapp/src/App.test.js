51cfea3a8ad563cecf8b695126238a9da464fb23
edielam <edguard738@gmail.com>
2022-09-03 10:32:21 +0000

    testing

--- cardatabase/frontend_React/carapp/src/App.test.js
+++ cardatabase/frontend_React/carapp/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import TestRenderer from 'react-test-renderer';
+import AddCar from './components/addCar';
 
 
 test('open add car modal form', () => {
@@ -7,4 +9,9 @@ test('open add car modal form', () => {
   fireEvent.click(screen.getByText('New Car'));
   expect(screen.getByRole('dialog')).toHaveTextContent
   ('New Car');
+});
+
+test('render a snapshot', () => {
+  const tree = TestRenderer.create(<AddCar/>).toJSON();
+  expect(tree).toMatchSnapshot();
 });
