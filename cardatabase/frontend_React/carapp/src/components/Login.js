abcca88d211f14eee48062bc2b5eb6f1d924358a
edielam <edguard738@gmail.com>
2022-09-03 11:36:27 +0000

    securing backend

--- cardatabase/frontend_React/carapp/src/components/Login.js
+++ cardatabase/frontend_React/carapp/src/components/Login.js
@@ -1,3 +1,12 @@
 import React from "react";
 import { SERVER_URL } from "../constants/constants";
 
+function Login() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Login;
